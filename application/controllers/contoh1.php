fbfc1fc3b8450620b6948c3bfde88fe0628384a9
Oniucup <denverheist87@gmail.com>
2024-03-20 21:49:15 +0700

    Added my project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Dava Sinatrio, saya mahasiswa semester 6 UBSI,
+        saya tinggal di Cikarang Barat, dan hobby saya yaitu dibidang otomotif.";
+    }
+}
